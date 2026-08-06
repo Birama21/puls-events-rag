656a3a98bd620c0bbb516648b60a97ccdf13cf4c
Birama21 <biramanedved@hotmail.fr>
2026-08-06 11:27:15 +0200

    Add CI v2

--- scripts/test_imports.py
+++
@@ -1,7 +0,0 @@
-import faiss
-
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-from mistralai.client import Mistral
-
-print("✅ Tous les imports fonctionnent !")
